a3a9dfef4b064b517e291c9a7d3f5b5932e1ba59
Odilon <odilon.vidal@icloud.com>
2022-05-28 01:01:45 +0200

    28/05

--- src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/vues/VueDuJeu.java
@@ -8,9 +8,12 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
@@ -46,7 +49,7 @@ public class VueDuJeu extends GridPane {
 
         plateau = new VuePlateau();
         destinations= new HBox();
-        passer = new Button("Passer");
+        passer = new Button("PASSER\nSON\nTOUR");
         joueurCourant = new VueJoueurCourant();
 
         VBox pioches = new VBox();
@@ -64,28 +67,47 @@ public class VueDuJeu extends GridPane {
         piocheDest.setGraphic(img);
         piocheDest.setStyle("-fx-background-color:transparent");
 
+
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setRadius(20.0);
+        dropShadow.setOffsetX(0);
+        dropShadow.setOffsetY(1.0);
+        dropShadow.setColor(Color.web("#F7E6BC"));
+        passer.setAlignment(Pos.CENTER);
+        passer.setPrefSize(120,100);
+        passer.setMaxSize(120,100);
+        passer.setFont(Font.font("Georgia", 20));
+        passer.setStyle("-fx-border-color: yellow");
+        passer.setStyle("-fx-background-color: #F7E6BC");
+        passer.setEffect(dropShadow);
+        pioche.setEffect(dropShadow);
+        piocheDest.setEffect(dropShadow);
+
+
+        pioches.setSpacing(50);
         pioches.setAlignment(Pos.CENTER);
-        pioches.getChildren().addAll(pioche, piocheDest);
+        pioches.getChildren().addAll(pioche, piocheDest,passer);
 
         //getChildren().addAll( destinations, passer, joueurCourant);
         ColumnConstraints premiercol=new ColumnConstraints();
         ColumnConstraints deuxiemecol=new ColumnConstraints();
         ColumnConstraints troisiemecol=new ColumnConstraints();
-        premiercol.setPercentWidth(22);
-        deuxiemecol.setPercentWidth(68);
-        troisiemecol.setPercentWidth(10);
+        premiercol.setPercentWidth(23);
+        deuxiemecol.setPercentWidth(66);
+        troisiemecol.setPercentWidth(14);
 
         RowConstraints premierl=new RowConstraints();
         RowConstraints deuxiemel=new RowConstraints();
         RowConstraints troisiemel=new RowConstraints();
-        premierl.setPercentHeight(5);
-        deuxiemel.setPercentHeight(90);
-        troisiemel.setPercentHeight(10);
+        premierl.setPercentHeight(10);
+        deuxiemel.setPercentHeight(80);
+        troisiemel.setPercentHeight(15);
 
         getColumnConstraints().addAll(premiercol, deuxiemecol, troisiemecol);
         getRowConstraints().addAll(premierl,deuxiemel,troisiemel);
+        setGridLinesVisible(true);
         add(destinations,1,2);
-        add(passer,2,2);
+        //add(passer,2,1);
         add(joueurCourant,0,1);
         add(plateau,1,1);
         add(pioches, 2,1);
--- src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
+++ src/main/java/fr/umontpellier/iut/vues/VueJoueurCourant.java
@@ -14,9 +14,13 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.PerspectiveTransform;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
@@ -29,16 +33,28 @@ import java.util.List;
  */
 public class VueJoueurCourant extends GridPane {
     private Label nomJoueur;
+    private Label fondnomJ;
     private VBox cartesG;
     private VBox cartesD;
     private VBox cartesDD;
 
+    private GridPane scrollPane;
+
     private ImageView photoJoueur;
+    private ImageView fondphotoJ;
 
     public VueJoueurCourant() {
+
+        fondnomJ = new Label();
+        fondnomJ.setFont(Font.font("Georgia", 30));
+        fondnomJ.setEffect(new GaussianBlur());
+
         nomJoueur = new Label();
-        nomJoueur.setFont(Font.font("Courrier", FontWeight.NORMAL, 30));
+        nomJoueur.setFont(Font.font("Georgia", 30));
         nomJoueur.setStyle("-fx-text-fill: white");
+
+        //nomJoueur.setFont(Font.loadFont("file:resources/fonts/IMFellEnglishSC-Regular.ttf", 30));
+
 
 
         cartesG = new VBox();
@@ -62,25 +78,31 @@ public class VueJoueurCourant extends GridPane {
         cartesD.setMaxSize(440, 440);
         //cartes.setAlignment(Pos.CENTER);
 
-        GridPane scrollPane = new GridPane();
+        scrollPane = new GridPane();
         scrollPane.setStyle("-fx-background-color: transparent");
-        scrollPane.setPrefSize(440, 440);
-        scrollPane.setMaxSize(440, 440);
+        scrollPane.setPrefSize(440, 500);
+        scrollPane.setMaxSize(440, 500);
+
         ColumnConstraints col1 = new ColumnConstraints();
         col1.setPercentWidth(33);
         ColumnConstraints col2 = new ColumnConstraints();
         col2.setPercentWidth(33);
         ColumnConstraints col3 = new ColumnConstraints();
-        col2.setPercentWidth(33);
+        col3.setPercentWidth(33);
         scrollPane.getColumnConstraints().addAll(col1,col2,col3);
         //scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.add(cartesG, 0,0);
         scrollPane.add(cartesD,1,0);
         scrollPane.add(cartesDD,2,0);
 
+        fondphotoJ = new ImageView();
+        fondphotoJ.setPreserveRatio(true);
+        fondphotoJ.setFitHeight(140);
+        fondphotoJ.setEffect(new GaussianBlur());
         photoJoueur = new ImageView();
         photoJoueur.setPreserveRatio(true);
         photoJoueur.setFitHeight(140);
+
 
         ColumnConstraints premiercol = new ColumnConstraints();
         premiercol.setPercentWidth(5);
@@ -101,10 +123,12 @@ public class VueJoueurCourant extends GridPane {
         ligne2.setPercentHeight(100);*/
         getColumnConstraints().addAll(premiercol, deuxiemecol, troisiemecol);
         getRowConstraints().addAll(ligne1, ligne2, ligne3);
-        setGridLinesVisible(true);
+
 
         add(nomJoueur, 1, 2);
+        add(fondnomJ, 1,2);
         add(scrollPane, 1, 3);
+        add(fondphotoJ, 1, 1);
         add(photoJoueur, 1, 1);
 
     }
@@ -114,9 +138,22 @@ public class VueJoueurCourant extends GridPane {
             @Override
             public void changed(ObservableValue<? extends IJoueur> observableValue, IJoueur iJoueur, IJoueur t1) {
                 Platform.runLater(() -> {
+                    DropShadow dropShadow = new DropShadow();
+                    dropShadow.setRadius(8.0);
+                    dropShadow.setOffsetX(0);
+                    dropShadow.setOffsetY(1.0);
+                    dropShadow.setColor(Color.web(couleurEnglish(t1.getCouleur().name())));
+
+                    scrollPane.setStyle("-fx-background-color: linear-gradient(from 0% 50% to 100% 50%, "+couleurEnglish(t1.getCouleur().name())+", rgba(244,244,244,0))");
+
+                    //scrollPane.setStyle("-fx-background-opacity: 0.5");
+                    fondnomJ.setText(t1.getNom());
                     nomJoueur.setText(t1.getNom());
+                    fondnomJ.setStyle("-fx-text-fill:" + couleurEnglish(t1.getCouleur().name()));
                     nomJoueur.setStyle("-fx-text-fill:" + couleurEnglish(t1.getCouleur().name()));
+
                     Image i1 = new Image("images/personnages/avatar-" + t1.getCouleur().name() + ".png");
+                    fondphotoJ.setImage(i1);
                     photoJoueur.setImage(i1);
                     cartesG.getChildren().clear();
                     cartesD.getChildren().clear();
@@ -141,7 +178,7 @@ public class VueJoueurCourant extends GridPane {
                         public void onChanged(Change<? extends ICouleurWagon> change) {
                             Platform.runLater(() -> {
                                 while (change.next()) {
-                                    if (change.wasAdded()) {
+                                    if (change.wasAdded() && !change.getAddedSubList().isEmpty()) {
                                         System.out.println(change.getAddedSubList().get(0).toString());
                                         VueCarteWagon vue = new VueCarteWagon(change.getAddedSubList().get(0));
                                         if(t1.cartesWagonProperty().size()<9) {
