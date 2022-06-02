9ecbc6d044640bd6b37e05ff3145e985bada8963
lillamanda <alexandrelillamand@gmail.com>
2022-06-02 10:00:12 +0200

    BRRR

--- src/main/java/fr/umontpellier/iut/vues/VueChoixJoueurs.java
+++ src/main/java/fr/umontpellier/iut/vues/VueChoixJoueurs.java
@@ -4,6 +4,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -40,8 +41,11 @@ public class VueChoixJoueurs extends Stage {
 
     public VueChoixJoueurs() {
         VBox vbox = new VBox();
+        HBox hbox = new HBox();
+        Button retirer = new Button();
         nomsJoueurs = FXCollections.observableArrayList();
         Button passer = new Button("Ajouter un joueur");
+
         Button start = new Button("JOUER");
         DropShadow dropShadow = new DropShadow();
         dropShadow.setRadius(20.0);
@@ -53,18 +57,31 @@ public class VueChoixJoueurs extends Stage {
         passer.setStyle("-fx-border-color: yellow");
         passer.setStyle("-fx-background-color: #F7E6BC");
         passer.setEffect(dropShadow);
-        passer.setPrefSize(300,80);
-        passer.setOnMouseEntered(mouseEvent -> {passer.setPrefSize(340,100);passer.setStyle("-fx-background-color: #F5E9D7");});
-        passer.setOnMouseExited(mouseEvent -> {passer.setPrefSize(300,80);passer.setStyle("-fx-background-color: #F7E6BC");});
+        passer.setPrefSize(400,80);
+        passer.setOnMouseEntered(mouseEvent -> {passer.setPrefSize(440,100);passer.setStyle("-fx-background-color: #F5E9D7");});
+        passer.setOnMouseExited(mouseEvent -> {passer.setPrefSize(400,80);passer.setStyle("-fx-background-color: #F7E6BC");});
 
         start.setAlignment(Pos.CENTER);
         start.setFont(Font.font("Georgia", 20));
         start.setStyle("-fx-border-color: green");
         start.setStyle("-fx-background-color: #0AD200");
+        DropShadow dp = new DropShadow();
+        dp.setRadius(20.0);
+        dp.setOffsetX(0);
+        dp.setOffsetY(1.0);
+        dp.setColor(Color.RED);
         start.setEffect(dropShadow);
         start.setPrefSize(300,80);
         start.setOnMouseEntered(mouseEvent -> {start.setPrefSize(360,120);start.setStyle("-fx-background-color: #2EE800");});
         start.setOnMouseExited(mouseEvent -> {start.setPrefSize(300,80);start.setStyle("-fx-background-color: #2EE800");});
+
+        retirer.setAlignment(Pos.CENTER_RIGHT);
+        retirer.setStyle("-fx-border-color: red");
+        retirer.setStyle("-fx-background-color: red");
+        retirer.setEffect(dp);
+        retirer.setPrefSize(80,70);
+        retirer.setOnMouseEntered(mouseEvent -> {retirer.setPrefSize(90,80);retirer.setStyle("-fx-background-color: red");});
+        retirer.setOnMouseExited(mouseEvent -> {retirer.setPrefSize(80,70);retirer.setStyle("-fx-background-color: red");});
 
         GridPane gridPane = new GridPane();
         Image bg = new Image("images/backgrounds/LUI.jpg");
@@ -85,11 +102,15 @@ public class VueChoixJoueurs extends Stage {
         RowConstraints deuxiemel=new RowConstraints();
         RowConstraints troisiemel=new RowConstraints();
         premierl.setPercentHeight(30);
-        deuxiemel.setPercentHeight(60);
+        deuxiemel.setPercentHeight(75);
+        troisiemel.setPercentHeight(20);
+
 
         gridPane.getColumnConstraints().addAll(premiercol, deuxiemecol, troisiemecol);
-        gridPane.getRowConstraints().addAll(premierl,deuxiemel);
+        gridPane.getRowConstraints().addAll(premierl,deuxiemel,troisiemel);
         gridPane.add(vbox, 1, 1);
+
+
 
         setMaximized(true);
         Scene s1 = new Scene(gridPane);
@@ -116,27 +137,85 @@ public class VueChoixJoueurs extends Stage {
         j4.setFont(Font.font("Georgia", 30));
         j5.setFont(Font.font("Georgia", 30));
 
+        Button espace = new Button("espace");
+        espace.setPrefSize(100,30);
+        espace.setOpacity(0);
+
+        hbox.setAlignment(Pos.CENTER);
+
+        hbox.setSpacing(25);
+
+
+        vbox.getChildren().addAll(j1,espace,hbox);
+
+        HBox hstart = new HBox();
+
+        hstart.setAlignment(Pos.CENTER);
+
+        gridPane.add(hstart,1,2);
 
 
 
-        vbox.getChildren().addAll(j1,j2,passer,start);
-        vbox.setSpacing(25);
+
+        hbox.getChildren().add(passer);
+        vbox.setSpacing(15);
+        hstart.setAlignment(Pos.TOP_CENTER);
 
 
 
         passer.setOnMouseClicked(event -> {
-            if(!vbox.getChildren().contains(j3)) {
-                vbox.getChildren().removeAll(passer,start);
-                vbox.getChildren().addAll(j3,passer,start);
+            if(!vbox.getChildren().contains(j2)) {
+                retirer.setOpacity(100);
+                vbox.getChildren().removeAll(espace,hbox);
+                if(!hbox.getChildren().contains(retirer)) {
+                    hbox.getChildren().add(retirer);
+                }
+                vbox.getChildren().addAll(j2,espace,hbox);
+                if(!hstart.getChildren().contains(start)){
+                    hstart.getChildren().add(start);
+                }
+
+            }
+            else if(!vbox.getChildren().contains(j3)) {
+                vbox.getChildren().removeAll(espace,hbox);
+                vbox.getChildren().addAll(j3,espace,hbox);
             }
             else if(!vbox.getChildren().contains(j4)){
-                vbox.getChildren().removeAll(passer,start);
-                vbox.getChildren().addAll(j4,passer,start);
+                vbox.getChildren().removeAll(espace,hbox);
+                vbox.getChildren().addAll(j4,espace,hbox);
             }
             else if(!vbox.getChildren().contains(j5)){
-                vbox.getChildren().removeAll(passer,start);
-                vbox.getChildren().addAll(j5,passer,start);
+
+                vbox.getChildren().removeAll(espace,hbox);
+                passer.setOpacity(0);
+                vbox.getChildren().addAll(j5,espace,hbox);
+                hbox.getChildren().remove(passer);
             }
+        });
+            retirer.setOnMouseClicked(eve -> {
+                if(vbox.getChildren().contains(j5)){
+                    hbox.getChildren().remove(retirer);
+                    if(!hbox.getChildren().contains(passer)) {
+                        hbox.getChildren().add(passer);
+                    }
+                    hbox.getChildren().add(retirer);
+                    passer.setOpacity(100);
+                    vbox.getChildren().remove(j5);
+                }
+                else if(vbox.getChildren().contains(j4)){
+                    vbox.getChildren().remove(j4);
+                }
+                else if(vbox.getChildren().contains(j3)){
+                    vbox.getChildren().remove(j3);
+                }
+                else if(vbox.getChildren().contains(j2)){
+                    vbox.getChildren().remove(j2);
+                    hbox.getChildren().remove(retirer);
+                    retirer.setOpacity(0);
+                    hstart.getChildren().remove(start);
+
+                }
+
 
                 });
         ArrayList<TextField> listefield = new ArrayList<>();
@@ -202,7 +281,7 @@ public class VueChoixJoueurs extends Stage {
      * Retourne le nombre de participants à la partie que l'utilisateur a renseigné
      */
     protected int getNombreDeJoueurs() {
-        throw new RuntimeException("Methode à implémenter");
+        return nomsJoueurs.size();
     }
 
     /**
@@ -210,7 +289,7 @@ public class VueChoixJoueurs extends Stage {
      * @param playerNumber : le numéro du participant
      */
     protected String getJoueurParNumero(int playerNumber) {
-        throw new RuntimeException("Methode à implémenter");
+        return nomsJoueurs.get(playerNumber);
     }
 
 }
